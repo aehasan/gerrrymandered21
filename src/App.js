f54bd57a8a73a16e0b699347197c185fecbecd15
ahmed <aehasan2@illinois.edu>
2021-04-02 23:32:12 -0500

    congressional regions displayed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import StartingMap from './Components/StartingMap';
+import React, { Component } from 'react';
+import * as d3 from 'd3'
 
 function App() {
+
+  var demHouseResults = {};
+  var repHouseResults = {};
+  // d3.csv("https://raw.githubusercontent.com/kjhealy/us_elections_2020_csv/master/results_current.csv").then(function(d, h) {
+  //     d.forEach(function(data, i) {
+  //         console.log(data.race)
+  //         if (String(data.party) === "Dem" && String(data.race) === "House") {
+  //             console.log("here")
+  //             demHouseResults[data.fips_char] = data.votes
+  //         }
+
+
+  //     })
+      
+  // }, function(error, rows) {
+  //     //console.log(rows)
+  //     //console.log(error)
+  // }
+  // )
+  // console.log(demHouseResults)
+
+
   return (
     <div className="App">
-      <StartingMap/>
+      <StartingMap dem={demHouseResults} rep={repHouseResults}/>
     </div>
   );
 }
--- src/Components/StartingMap.js
+++ src/Components/StartingMap.js
@@ -4,19 +4,25 @@ import {useEffect, useRef, useState} from 'react';
 import * as d3 from 'd3'
 import * as topojson from "topojson";
 import ReactDOM from 'react-dom';
+import countyData from '/Users/ahmed/Desktop/gerrymandered/src/data/congressional.json'
 import data from '/Users/ahmed/Desktop/gerrymandered/src/data/counties.json'
+
 
 
 import  "/Users/ahmed/Desktop/gerrymandered/src/styling.css"
 
-function StartingMap() {
+
+function StartingMap(props) {
     const wrapperRef = useRef()
     //svg.attr('width', 1500).attr('height', 500)
-    console.log(data)
+    //console.log(data)
 
-    var j = data;
+    //var j = data;
 
     const svgRef = useRef();
+
+
+    //console.log(repHouseResults)
 
     
 
@@ -27,19 +33,20 @@ function StartingMap() {
  
         const projection = d3.geoAlbersUsa()
         const pathGenerator = d3.geoPath().projection(projection);
+        //console.log(data.objects)
     
 
-       var counties = topojson.feature(data, data.objects.counties).features
+       var counties = topojson.feature(countyData, countyData.objects.cb_2018_us_cd116_500k).features
        var states = topojson.feature(data, data.objects.states).features
 
         
         svg.selectAll(".counties").data(counties).join("path").attr('class', 'counties').attr("d", feautre => pathGenerator(feautre))
         .on("mouseover", (data, index) => (console.log(index)))
         svg.selectAll(".states").data(states).join("path").attr('class', 'states').attr("d", feautre => pathGenerator(feautre)).attr("opacity", 0).on("mouseover", (data, index) => (console.log(index.properties.name)))
+        //console.log(demHouseResults)
 
 
-
-    },[j]);
+    },[]);
 
     return (
 
